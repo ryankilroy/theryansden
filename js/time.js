ecf576df80bf17d0782cfe86df7e85a70a7feb81
Ryan Collins <ryanmattcollins@gmail.com>
2015-10-16 00:28:10 -0400

    Added contact info and temporary message. Fixed some clock functionality.

--- js/time.js
+++ js/time.js
@@ -9,7 +9,17 @@ function updateTime() {
     seconds = current.getSeconds();
     seconds = seconds < 10 ? '0' + seconds : seconds;
     suffix = hours >= 12 ? 'pm' : 'am';
+    
+    //No one should be looking at this site this early in the morning...
+    if (hours < 6 && hours > 2) {
+        hours = 'why are';
+        minutes = 'you still';
+        seconds = 'awake !?';
+    }
+    
     hours = suffix === 'pm' ? hours - 12 : hours;
+    hours = hours <= 0 ? 12 : hours;
+    hours = hours < 10 ? '0' + hours : hours;
     formatTime = hours + ':' + minutes + ':' + seconds + ' ' + suffix;
     document.getElementById("hrLabel").innerHTML = hours;
     document.getElementById("minLabel").innerHTML = minutes;
