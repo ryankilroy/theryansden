528af80b23138b4b2edefae2f8b997d9adfeb6ec
Ryan Collins <ryan.matt.collins@gmail.com>
2016-01-27 09:29:47 -0500

    Returning floater, chatroom is probably in broken state

--- chatroom/chatroom.rb
+++ chatroom/chatroom.rb
@@ -1,14 +1,40 @@
 require 'sinatra'
 require 'yaml/store'
+require 'cgi'
 
 get '/' do
 	@title = "Welcome to The Ryan's Den"
 	erb :index
 end
 
-get '/chat/login' do
+get '/chat' do
+	cgi = CGI.new("html4")
+	cookie = cgi.cookies['session_cookie']
+	puts "cookie: "
+   	puts cookie
+	cgi.out('cookie' => cookie) do
+		cgi.head + cgi.body { cookie[0] }
+	end
+
 	@title = "Please login or register"
 	erb :login
+end
+
+get '/chat/signup' do
+	@handle = params['handle']
+	@users = YAML::Store.new 'users.yml'
+	@users.transaction do
+		if (@users[@handle])
+			puts "#{@handle} already exists"
+			@title = 'User exists'
+			erb :error
+		else
+			@password = params['password']
+			@users[@handle] = @password
+			@title = "Welcome to the club #{@handle}!"
+			erb :loginSuccess
+		end
+	end
 end
 
 post '/chat/login' do
@@ -16,17 +42,24 @@ post '/chat/login' do
 	@password = params['password']
 	@users = YAML::Store.new 'users.yml'
 	@users.transaction do
-		if (@users[@handle] && @users[@handle] != @password)
+		if (!@users[@handle])
+			puts "#{@handle} does not exist, creating user"
+			@users[@handle] = @password
+			#redirect_to '/signup'
+		elsif (@users[@handle] && @users[@handle] != @password)
 			puts "#{@handle}: Wrong Password"
 			@title = 'Wrong Password'
 			erb :error
-		elsif (!@users[@handle])
-			@users[@handle] = @password
-			@title = "Welcome newcomer #{@handle}!"
-			erb :loginSuccess
 		elsif (@users[@handle] == @password)	
 			@title = "Welcome back #{@handle}!"
-			erb :loginSuccess
+			cgi = CGI.new("html4")
+			cookie = CGI::Cookie.new('name' => 'session_cookie',
+									 'value' => @handle,
+									 'expires' => Time.now + 60)
+			cgi.out('cookie' => cookie) do
+				cgi.head + cgi.body { "#{cookie} stored"}
+			end
+			#erb :loginSuccess
 		end
 	end
 end
